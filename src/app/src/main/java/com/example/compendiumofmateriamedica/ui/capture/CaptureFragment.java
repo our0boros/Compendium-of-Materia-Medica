209308bb8e50d4b9fd23cbec50d76cfcc12fe60c
Hongjun Xu <u7733037@anu.edu.au>
2024-04-27 15:36:39 +1000

    [UPDATE] A Capture UI draft

--- src/app/src/main/java/com/example/compendiumofmateriamedica/ui/capture/CaptureFragment.java
+++ src/app/src/main/java/com/example/compendiumofmateriamedica/ui/capture/CaptureFragment.java
@@ -1,4 +1,4 @@
-package com.example.compendiumofmateriamedica.ui.dashboard;
+package com.example.compendiumofmateriamedica.ui.capture;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.compendiumofmateriamedica.databinding.FragmentDashboardBinding;
+import com.example.compendiumofmateriamedica.databinding.FragmentCaptureBinding;
 
-public class DashboardFragment extends Fragment {
+public class CaptureFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentCaptureBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        CaptureViewModel captureViewModel =
+                new ViewModelProvider(this).get(CaptureViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentCaptureBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        captureViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
