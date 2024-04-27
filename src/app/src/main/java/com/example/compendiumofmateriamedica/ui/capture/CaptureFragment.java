7ae2098947fb518ad361293c76f0a43314ad4bd6
Hongjun Xu <u7733037@anu.edu.au>
2024-04-27 16:02:25 +1000

    [UPDATE] Token.java with Type {TAG, TEXT, LBRACE, RBRACE, SEP, COLON, STR}

--- src/app/src/main/java/com/example/compendiumofmateriamedica/ui/capture/CaptureFragment.java
+++ src/app/src/main/java/com/example/compendiumofmateriamedica/ui/capture/CaptureFragment.java
@@ -4,17 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.compendiumofmateriamedica.R;
 import com.example.compendiumofmateriamedica.databinding.FragmentCaptureBinding;
 
+import java.util.Objects;
+
+/**
+ * @author: Hongjun Xu
+ * @datetime: 2024/4/27
+ * @description: Capture Page
+ */
 public class CaptureFragment extends Fragment {
 
     private FragmentCaptureBinding binding;
+    private EditText searchText;
+    private ImageButton captureButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,7 +38,16 @@ public class CaptureFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         captureViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        searchText = requireView().findViewById(R.id.searchBarText);
+        captureButton = requireView().findViewById(R.id.captureButton);
+
+        captureButton.setOnClickListener(this::OnClick);
         return root;
+    }
+
+    public void OnClick(View v) {
+
     }
 
     @Override
---
+++ src/app/src/main/java/model/Token.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.Objects;
+
+/**
+ * Token class to save extracted token from tokenizer.
+ * Each token has its surface form saved in {@code token}
+ * and type saved in {@code type} which is one of the predefined type in Type enum.
+ * The following are the different types of tokens:
+ *
+ *  Search Grammar: #: {TAG1, TAG2, TAG3}, $: {TEXT1, TEXT2, TEXT3}, *: {&}
+ *  if * search method is &-and: find TAG1: TEXT1 and TAG2: TEXT2 and ...
+ *  if * search method is |-or: find TAG1: TEXT1 or TAG2: TEXT2 and ...
+ *
+ * TAG: #
+ * TEXT: $
+ * LBRACE: {
+ * RBRACE: }
+ * SEP: ,
+ * COLON: :
+ * STR: TEXT/ TAG/ ...
+ */
+public class Token {
+    // The following enum defines different types of tokens. Example of accessing these: Token.Type.INT
+    public enum Type {TAG, TEXT, LBRACE, RBRACE, SEP, COLON, STR}
+
+    /**
+     * The following exception should be thrown if a tokenizer attempts to tokenize something that is not of one
+     * of the types of tokens.
+     */
+    public static class IllegalTokenException extends IllegalArgumentException {
+        public IllegalTokenException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    // Fields of the class Token.
+    private final String token; // Token representation in String form.
+    private final Type type;    // Type of the token.
+
+    public Token(String token, Type type) {
+        this.token = token;
+        this.type = type;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        if (type == Type.STR) {
+            return "STR(" + token + ")";
+        } else {
+            return type + "";
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true; // Same hashcode.
+        if (!(other instanceof Token)) return false; // Null or not the same type.
+        return this.type == ((Token) other).getType() && this.token.equals(((Token) other).getToken()); // Values are the same.
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(token, type);
+    }
+}
