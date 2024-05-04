ef46d7e63e93bfc63d8f49ee801bc40a9846e243
Hongjun Xu <u7733037@anu.edu.au>
2024-05-04 18:39:33 +1000

    [UPDATE] add user image and name

--- src/app/src/main/java/model/RowAdapter.java
+++ src/app/src/main/java/model/RowAdapter.java
@@ -23,11 +23,13 @@ public class RowAdapter extends RecyclerView.Adapter<RowAdapter.RowViewHolder> {
     private Context context;
     private ArrayList<Integer> data;
     private PostTreeManager postTreeManager;
+    private UserTreeManager userTreeManager;
 
     public RowAdapter(Context context, ArrayList<Integer> data) throws JSONException, IOException {
         this.context = context;
         this.data = data;
         postTreeManager = new PostTreeManager((RBTree<Post>) GeneratorFactory.tree(this.context, DataType.POST, R.raw.posts));
+        userTreeManager = new UserTreeManager((RBTree<User>) GeneratorFactory.tree(this.context, DataType.USER, R.raw.users));
     }
 
     @Override
@@ -40,11 +42,20 @@ public class RowAdapter extends RecyclerView.Adapter<RowAdapter.RowViewHolder> {
     public void onBindViewHolder(@NonNull RowViewHolder holder, int position) {
         ArrayList<RBTreeNode<Post>> nodes = postTreeManager.search(PostTreeManager.PostInfoType.POST_ID, String.valueOf(data.get(position)));
         Log.println(Log.ASSERT, "DEBUG", "[GridAdapter] onBindViewHolder: nodes size " + nodes.size());
+        // 加载Post图片
         String postURL = nodes.get(0).getValue().getPhoto();
         MainActivity.loadImageFromURL(this.context, postURL, holder.postImage, "Photo");
-        String postContent = nodes.get(0).getValue().getContent();
+        // 加载用户图片
+        RBTreeNode<User> user = userTreeManager.search(UserTreeManager.UserInfoType.ID, nodes.get(0).getValue().getUid()).get(0);
+        String userURL = user.getValue().getAvatar();
+        Log.println(Log.ASSERT, "DEBUG", "[GridAdapter] onBindViewHolder: user avatar: " + userURL);
+        MainActivity.loadImageFromURL(this.context, userURL, holder.userImage, "Avatar");
+        // 加载用户名字
+        holder.userName.setText(user.getValue().getName());
 
+        String postContent = nodes.get(0).getValue().getContent();
         holder.postContent.setText(postContent);
+
 
     }
 
@@ -55,6 +66,7 @@ public class RowAdapter extends RecyclerView.Adapter<RowAdapter.RowViewHolder> {
 
     public static class RowViewHolder extends RecyclerView.ViewHolder {
         public ImageView postImage;
+        public ImageView userImage;
         public TextView userName;
         public TextView postContent;
 
@@ -62,6 +74,7 @@ public class RowAdapter extends RecyclerView.Adapter<RowAdapter.RowViewHolder> {
         public RowViewHolder(View itemView) {
             super(itemView);
             postImage = itemView.findViewById(R.id.rowLayoutPostImage);
+            userImage = itemView.findViewById(R.id.rowLayoutUserImage);
             userName = itemView.findViewById(R.id.rowLayoutUserName);
             postContent = itemView.findViewById(R.id.rowLayoutPostContent);
         }
