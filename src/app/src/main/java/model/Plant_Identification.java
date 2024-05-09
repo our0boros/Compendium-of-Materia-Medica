72d1b8573a113028f800bd3d0bc19118d31e8e68
Hongjun Xu <u7733037@anu.edu.au>
2024-05-09 23:44:16 +1000

    [UPDATE] fix fake result

--- src/app/src/main/java/model/Plant_Identification.java
+++ src/app/src/main/java/model/Plant_Identification.java
@@ -6,8 +6,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.Base64;
 
 public class Plant_Identification {
@@ -62,7 +64,7 @@ public class Plant_Identification {
 		System.out.println("Response: " + response.toString());
 	}
 
-	public static String getAPIResult(String imagePath) throws IOException {
+	public static String getPlantIDAPIResult(String imagePath) throws IOException {
 		String base64Image = imageToBase64(imagePath);
 
 		String url = "https://plant.id/api/v3/identification";
@@ -103,4 +105,49 @@ public class Plant_Identification {
 		return response.toString();
 	}
 
+	public static String getPlantNetAPIResult(String imagePath) {
+		StringBuilder response = new StringBuilder();
+		String API_KEY = "2b10sgwYhB8pSqL6gMuqa3R";
+		String PROJECT = "all"; // 尝试特定的植物区域: "weurope", "canada"…
+		String API_ENDPOINT = "https://my-api.plantnet.org/v2/identify/" + PROJECT + "?api-key=" + API_KEY;
+
+		try {
+			// 读取图像文件并转换为字节数组
+			byte[] imageData = Files.readAllBytes(new File(imagePath).toPath());
+
+			// 构建 HTTP 请求
+			HttpURLConnection connection = (HttpURLConnection) new URL(API_ENDPOINT).openConnection();
+			connection.setRequestMethod("POST");
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "multipart/form-data");
+			// 构建请求体
+			try (OutputStream out = connection.getOutputStream()) {
+				// 写入图像数据
+				out.write(("--boundary\r\n").getBytes());
+				out.write(("Content-Disposition: form-data; name=\"images\"; filename=\"image_1.jpeg\"\r\n").getBytes());
+				out.write(("Content-Type: image/jpeg\r\n\r\n").getBytes());
+				out.write(imageData);
+				out.write(("\r\n--boundary\r\n").getBytes());
+			}
+
+			// 发送请求并获取响应
+			int responseCode = connection.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					response.append(line);
+				}
+				reader.close();
+//				System.out.println("Response Code: " + responseCode);
+//				System.out.println("Response Body:\n" + response.toString());
+			} else {
+//				System.out.println("Request failed: " + responseCode + " " + connection.getResponseMessage());
+				return "ERROR" + responseCode + " " + connection.getResponseMessage();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return response.toString();
+	}
 }
