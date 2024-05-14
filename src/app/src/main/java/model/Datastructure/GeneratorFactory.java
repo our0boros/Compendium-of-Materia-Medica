0394e172ca730759695981aab771ac4d8a556cc0
HaochenGong <u7776634@anu.edu.au>
2024-05-14 21:26:49 +1000

    [UPDATE] change package location

--- src/app/src/main/java/model/Datastructure/GeneratorFactory.java
+++ src/app/src/main/java/model/Datastructure/GeneratorFactory.java
@@ -1,4 +1,4 @@
-package model;
+package model.Datastructure;
 
 import android.content.Context;
 
@@ -8,11 +8,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import model.Datastructure.DataType;
-import model.Datastructure.PlantTreeGenerator;
-import model.Datastructure.PostTreeGenerator;
-import model.Datastructure.RBTree;
-import model.Datastructure.UserTreeGenerator;
+import model.JsonReader;
 
 /**
  * @author: Haochen Gong
